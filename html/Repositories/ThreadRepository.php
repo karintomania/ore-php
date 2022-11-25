1a91e857303c2c7fdcbdab6c606707e55df50611
karintomania <19652340+karintomania@users.noreply.github.com>
2022-11-25 09:14:17 +0000

    add thread repository

--- html/Repositories/ThreadRepository.php
+++ html/Repositories/ThreadRepository.php
@@ -3,7 +3,25 @@
 namespace Repositories;
 
 class ThreadRepository{
-	function test(){
+
+	function __construct(private \PDO $pdo){}
+
+	function findById(int $id): array{
+		$stmt = $this->pdo->prepare('
+			SELECT * FROM threads
+			WHERE id = :id');
+		$stmt->execute(['id' => $id]);
+
+		return $stmt->fetch();
+	}
+	function findAll(){
+	}
+	function create(string $name): int{
+		$stmt = $this->pdo->prepare('
+			INSERT INTO threads (name)
+			VALUES (:name)');
+		$stmt->execute(['name' => $name]);
+		return $this->pdo->lastInsertId();
 	}
 
 }
--- html/Tests/Repositories/ThreadRepositoryTest.php
+++ html/Tests/Repositories/ThreadRepositoryTest.php
@@ -3,11 +3,40 @@
 namespace Tests\Repositories;
 
 use OreFramework\Tests\Test;
+use Repositories\ThreadRepository;
 
 class ThreadRepositoryTest extends Test{
 
-	function test_returns_true(){
-		$this->assertEquals(1, 2);
+	function __construct(private ThreadRepository $tr){}
+
+	function test_create_creates_thread(){
+		$threadName = 'test thread';
+
+		$threadId = $this->tr->create($threadName);
+
+		$thread = $this->tr->findById($threadId);
+
+		$this->assertEquals($threadId, $thread['id']);
+		$this->assertEquals($threadName, $thread['name']);
+		$this->assertEquals(date('Y-m-d H:i:s'), $thread['createdAt']);
+
+	}
+
+	function test_create_creates_thread_with_japanese_name(){
+		$threadName = 'テストスレ';
+
+		$threadId = $this->tr->create($threadName);
+
+		$thread = $this->tr->findById($threadId);
+
+		// $this->assertEquals($threadId, $thread['id']);
+		// $this->assertEquals($threadName, $thread['name']);
+
+	}
+	function test_findAll_returns_all_threads(){
+		// $threads = $this->tr->findAll();
+
+		// $this->assertEquals(1, 1);
 	}
 
 }
