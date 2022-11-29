20edfca86e9fc60acb6cd55698458a4b71656ab0
karintomania <19652340+karintomania@users.noreply.github.com>
2022-11-29 21:57:01 +0000

    add environment variable for DB

--- html/resolveMap.php
+++ html/resolveMap.php
@@ -3,10 +3,10 @@
 
 return [
 	\PDO::class => function(){
-		$hostname = 'vanilla-php-db';
-		$dbname = 'app';
-		$password = 'root';
-		$user = 'root';
+		$hostname = $_ENV['DB_HOST'];
+		$dbname   = $_ENV['DB_NAME'];
+		$user     = $_ENV['DB_USER'];
+		$password = $_ENV['DB_PASSWORD'];
 
 		return new PDO(
 			"mysql:host={$hostname};dbname={$dbname}",
